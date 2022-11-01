03977255f3df62b931ccd53bd730162934603c73
peiyao2017 <104920191+peiyao2017@users.noreply.github.com>
2022-11-01 18:31:55 -0500

    Add files via upload

---
+++ R functions/table2/app cusum.R
@@ -0,0 +1,210 @@
+
+
+
+app_cusum=function(m=100,d=4,threshold=200,mu0=rep(0,times=d),mu1=rep(0.5,times=d),covar=diag(1,d,d)){
+  c=0
+  y0=mvrnorm(n=m,mu=mu0,Sigma=covar)
+  y1=mvrnorm(n=m,mu=mu1,Sigma=covar)
+  estmu0=colMeans(y0)
+  estmu1=colMeans(y1)
+  estcovar0=cov(y0)
+  estcovar1=cov(y1)
+  repeat{
+    x=mvrnorm(n=1,mu=mu0,Sigma = covar)
+    c=c+1
+    print(c)
+    x1=numeric()
+    x2=numeric()
+    c1=0
+    repeat{
+      if(c1==0){
+        x2[1]=log(dmvnorm(x,mean = estmu1,sigma = estcovar1)/dmvnorm(x,mean = estmu0,sigma = estcovar0))
+        x1[1]=x2[1]
+      }
+      print(max(x1))
+      if(max(x1)>threshold){
+        break
+      }
+      if(max(x1)<0){
+        break
+      }
+      if(c1>=1){
+        x2[length(x2)+1]=log(dmvnorm(x[nrow(x),],mean = estmu1,sigma = estcovar1)/dmvnorm(x[nrow(x),],mean = estmu0,sigma = estcovar0))
+        for(i in 1:length(x2)){
+          x1[i]=sum(x2[i:length(x2)])
+        }
+      }
+      print(max(x1))
+      if(max(x1)>threshold){
+        break
+      }
+      if(max(x1)<0){
+        break
+      }
+      x=rbind(x,mvrnorm(n=1,mu=mu0,Sigma = covar))
+      c=c+1
+      c1=c1+1
+      print(c) 
+    }
+    if(max(x1)>threshold){
+      break
+    }
+    if(c>1000){
+      break
+    }
+  }
+  return(c)
+}
+
+
+
+app1_cusum=function(m=100,d=4,threshold=200,mu0=rep(0,times=d),mu1=rep(0.5,times=d),covar=diag(1,d,d)){
+  false=0
+  c=0
+  y0=mvrnorm(n=m,mu=mu0,Sigma=covar)
+  y1=mvrnorm(n=m,mu=mu1,Sigma=covar)
+  estmu0=colMeans(y0)
+  estmu1=colMeans(y1)
+  estcovar0=cov(y0)
+  estcovar1=cov(y1)
+  repeat{
+    repeat{
+      x=mvrnorm(n=1,mu=mu0,Sigma = covar)
+      c=c+1
+      print(c)
+      x1=numeric()
+      x2=numeric()
+      c1=0
+      repeat{
+        if(c1==0){
+          x2[1]=log(dmvnorm(x ,mean = estmu1,sigma = estcovar1)/dmvnorm(x ,mean = estmu0,sigma = estcovar0))
+          x1[1]=x2[1]
+        }
+        print(max(x1))
+        if(max(x1)>threshold){
+          false=false+1
+          break
+        }
+        if(max(x1)<0){
+          break
+        }
+        if(c1>=1){
+          x2[length(x2)+1]=log(dmvnorm(x[nrow(x),],mean = estmu1,sigma = estcovar1)/dmvnorm(x[nrow(x),],mean = estmu0,sigma = estcovar0))
+          for(i in 1:length(x2)){
+            x1[i]=sum(x2[i:length(x2)])
+          }
+        }
+        print(max(x1))
+        if(max(x1)>threshold){
+          false=false+1
+          break
+        }
+        if(max(x1)<0){
+          break
+        }
+        x=rbind(x,mvrnorm(n=1,mu=mu0,Sigma = covar))
+        c=c+1
+        c1=c1+1
+        print(c) 
+        if(c>=49){
+          break
+        }
+        if(false>=10){
+          break
+        }
+      }
+      if(max(x1)>threshold){
+        break
+      }
+      if(c>=49){
+        break
+      }
+      if(false>=10){
+        break
+      }
+    }
+    if(c>=49){
+      break
+    }
+    if(false>=10){
+      break
+    }
+  }
+  if(false<10){
+  nodetect=0
+  repeat{
+    x=rbind(x,mvrnorm(n=1,mu=mu1,Sigma = covar))
+    c=c+1
+    x2[length(x2)+1]=log(dmvnorm(x[nrow(x),],mean = estmu1,sigma = estcovar1)/dmvnorm(x[nrow(x),],mean = estmu0,sigma = estcovar0))
+    for(i in 1:length(x2)){
+      x1[i]=sum(x2[i:length(x2)])
+    }
+    if(max(x1)>threshold){
+      break
+    }
+    if(max(x1)<0){
+      break
+    }
+    if(c>1000){
+      nodetect=1
+      break
+    }
+  }
+  
+  if(max(x1)<=threshold){
+    repeat{
+      x=mvrnorm(n=1,mu=mu1,Sigma = covar)
+      c=c+1
+      print(c)
+      x1=numeric()
+      x2=numeric()
+      c1=0
+      repeat{
+        if(c1==0){
+          x2[1]=log(dmvnorm(x ,mean = estmu1,sigma = estcovar1)/dmvnorm(x ,mean = estmu0,sigma = estcovar0))
+          x1[1]=x2[1]
+        }
+        print(max(x1))
+        if(max(x1)>threshold){
+          break
+        }
+        if(max(x1)<0){
+          break
+        }
+        if(c1>=1){
+          x2[length(x2)+1]=log(dmvnorm(x[nrow(x),],mean = estmu1,sigma = estcovar1)/dmvnorm(x[nrow(x),],mean = estmu0,sigma = estcovar0))
+          for(i in 1:length(x2)){
+            x1[i]=sum(x2[i:length(x2)])
+          }
+        }
+        print(max(x1))
+        if(max(x1)>threshold){
+          break
+        }
+        if(max(x1)<0){
+          break
+        }
+        x=rbind(x,mvrnorm(n=1,mu=mu1,Sigma = covar))
+        c=c+1
+        c1=c1+1
+        print(c) 
+        if(c>1000){
+          nodetect=1
+          break
+        }
+      }
+      if(max(x1)>threshold){
+        break
+      }
+      if(c>1000){
+        nodetect=1
+        break
+      }
+    }
+  }
+  return(c(c-50,nodetect))
+  }
+  if(false>=10){
+    return("false")
+  }
+}
